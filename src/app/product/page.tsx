0045d1fbe92fcb7029f8024d453cc4e07235c7b7
enes yasin gedik <gedikas@hotmail.com>
2024-07-07 21:36:09 +0300

    added product page

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -31,16 +31,45 @@ const Product = () => {
 				</nav>
 			</header>
 			<main className={styles.main}>
-				<div className={styles.overlay}>
-					<h1 className={styles.title}>Welcome to Craft Industrial</h1>
-					<p className={styles.subtitle}>
-						Craf Industrial was established to manufacture and supply high
-						quality machines and auxiliary equipment to industrial companies.
-						You can trust Craft Industrial to deliver low-cost, durable and
-						long-lasting solutions to meet the production demands of an
-						ever-changing business environment.
-					</p>
-					<button className={styles.contactButton}>CONTACT NOW</button>
+				<div className={styles.productContainer}>
+					<div className={styles.product}>
+						<div className={styles.productTitle}>AIR WIPES</div>
+						<img
+							src="/Rectangle17.png"
+							alt="Air Wipes"
+							className={styles.productImage}
+						/>
+					</div>
+					<div className={styles.product}>
+						<div className={styles.productTitle}>BLADES</div>
+						<img
+							src="/Rectangle18.png"
+							alt="Blades"
+							className={styles.productImage}
+						/>
+					</div>
+					<div className={styles.product}>
+						<div className={styles.productTitle}>STANDS</div>
+						<img
+							src="/Rectangle19.png"
+							alt="Stands"
+							className={styles.productImage}
+						/>
+					</div>
+					<div className={styles.product}>
+						<div className={styles.productTitle}>WELDERS</div>
+						<img
+							src="/Rectangle20.png"
+							alt="Welders"
+							className={styles.productImage}
+						/>
+					</div>
+				</div>
+				<div className={styles.customMachines}>
+					<button className={styles.customButton}>CUSTOM MACHINES</button>
+					<div className={styles.customText}>
+						Call to discuss, and receive a free quote.
+					</div>
 				</div>
 			</main>
 		</div>
