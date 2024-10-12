b85cdf13c19bcaa155a5f16fbda3e2958ac394d7
Argedik <gedikas@hotmail.com>
2024-10-12 22:03:20 +0300

    Finished footer area

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,74 +1,74 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { PrismaClient } from '@prisma/client';
-import nodemailer from 'nodemailer';
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+import nodemailer from "nodemailer";
 
 const prisma = new PrismaClient();
 
 export default async function handler(
-	req: NextApiRequest,
-	res: NextApiResponse
+    req: NextApiRequest,
+    res: NextApiResponse
 ) {
-	if (req.method !== 'POST') {
-		return res
-			.status(405)
-			.json({ message: 'Yalnızca POST istekleri kabul edilir.' });
-	}
+    if (req.method !== "POST") {
+        return res
+            .status(405)
+            .json({ message: "Yalnızca POST istekleri kabul edilir." });
+    }
 
-	const { name, surname, email, phone, message } = req.body;
+    const { name, surname, email, phone, message } = req.body;
 
-	try {
-		const existingMail = await prisma.mail.findFirst({
-			where: {
-				name,
-				surname,
-				email,
-			},
-		});
+    try {
+        const existingMail = await prisma.mail.findFirst({
+            where: {
+                name,
+                surname,
+                email,
+            },
+        });
 
-		if (existingMail) {
-			return res.status(409).json({ message: 'Bu kişi zaten var.' });
-		}
+        if (existingMail) {
+            return res.status(409).json({ message: "Bu kişi zaten var." });
+        }
 
-		const mail = await prisma.mail.create({
-			data: {
-				name,
-				surname,
-				email,
-				phone,
-				message,
-			},
-		});
+        const mail = await prisma.mail.create({
+            data: {
+                name,
+                surname,
+                email,
+                phone,
+                message,
+            },
+        });
 
-		let transporter = nodemailer.createTransport({
-			service: 'Gmail',
-			auth: {
-				user: process.env.GMAIL_USER,
-				pass: process.env.GMAIL_PASS,
-			},
-		});
+        let transporter = nodemailer.createTransport({
+            service: "Gmail",
+            auth: {
+                user: process.env.GMAIL_USER,
+                pass: process.env.GMAIL_PASS,
+            },
+        });
 
-		let mailOptions = {
-			from: 'test343665@gmail.com',
-			// to: 'tugaygundem@gmail.com', //Contact sayfasında doldurulan formun gönderileceği mail adresi
-			to: 'gedikas@hotmail.com', //Contact sayfasında doldurulan formun gönderileceği mail adresi
-			subject: 'Tugay hocam maillerimiz başladı. Hayırlı olsun :)',
-			html: `
+        let mailOptions = {
+            from: "test343665@gmail.com",
+            // to: 'tugaygundem@gmail.com', //Contact sayfasında doldurulan formun gönderileceği mail adresi
+            to: "gedikas@hotmail.com", //Contact sayfasında doldurulan formun gönderileceği mail adresi
+            subject: "Tugay hocam maillerimiz başladı. Hayırlı olsun :)",
+            html: `
         <h3>Yeni müşteri potansiyeli mi diyelim ne yazalım hocam </h3>
         <p><strong>İsim:</strong> ${name}</p>
         <p><strong>Soyisim:</strong> ${surname}</p>
         <p><strong>Email:</strong> ${email}</p>
         <p><strong>Telefon:</strong> ${phone}</p>
         <p><strong>Mesaj:</strong> ${message}</p>
-      `,
-		};
+    `,
+        };
 
-		await transporter.sendMail(mailOptions);
+        await transporter.sendMail(mailOptions);
 
-		res.status(200).json({ message: 'Mesaj başarıyla gönderildi.' });
-	} catch (error) {
-		console.error(error);
-		res.status(500).json({ message: 'Bir hata oluştu.' });
-	} finally {
-		await prisma.$disconnect();
-	}
+        res.status(200).json({ message: "Mesaj başarıyla gönderildi." });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Bir hata oluştu." });
+    } finally {
+        await prisma.$disconnect();
+    }
 }
--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -1,60 +1,159 @@
 import Link from "next/link";
 import styles from "./styles.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faInstagram,
+    faLinkedin,
+    faSquareInstagram,
+    faSquareWhatsapp,
+    faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <div className={`${styles.footerContainer}`}>
-                <div className={`${styles.firstContainer} `}>
-                    <div className={styles.footerColumn}>
-                        <h3 className={styles.footerTitle}>
-                            Craft Industrial Design
-                        </h3>
-                        <p>2024 All rights Reserved</p>
+            <div className={styles.container}>
+                <div className={styles.brand}>
+                    <h3 className={styles.title}>Craft Industrial Design</h3>
+                    <div>
+                        <p className={styles.known}>2024 All rights Reserved</p>
                         <p>
                             Craft Industrial, headquartered in Istanbul, Turkey,
                             is a new and....
                         </p>
-                        <div className={styles.footerSocial}>
-                            <Link href="https://linkedin.com">LinkedIn</Link>
-                            <Link href="https://instagram.com">Instagram</Link>
-                            <Link href="https://whatsapp.com">WhatsApp</Link>
+                        <p className={`${styles.known} ${styles.info}`}>
+                            <Link
+                                href="https://artmob.com.tr/en/kvkk"
+                                className={styles.infoLink}
+                            >
+                                KVKK
+                            </Link>
+                            <span className={styles.bracket}>|</span>
+                            <Link
+                                href="https://artmob.com.tr/en/kullanim-sartlari"
+                                className={styles.infoLink}
+                            >
+                                Terms of use
+                            </Link>
+                            <span className={styles.bracket}>|</span>
+                            <Link
+                                href="https://artmob.com.tr/en/gizlilik"
+                                className={styles.infoLink}
+                            >
+                                Privacy Policy
+                            </Link>
+                            <span className={styles.bracket}>|</span>
+                            <Link
+                                href="https://artmob.com.tr/en/cerezler"
+                                className={styles.infoLink}
+                            >
+                                Cookies
+                            </Link>
+                            <span className={styles.bracket}>|</span>
+                        </p>
+                        <div className={styles.social}>
+                            <a
+                                href="https://www.linkedin.com"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <img
+                                    src="/linkedin.svg"
+                                    alt="WhatsApp"
+                                    className={styles.icon}
+                                />
+                            </a>
+
+                            <a
+                                href="https://www.instagram.com"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <img
+                                    src="/instagram.svg"
+                                    alt="WhatsApp"
+                                    className={styles.icon}
+                                />
+                            </a>
+                            <a
+                                href="https://web.whatsapp.com/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <img
+                                    src="/whatsapp.svg"
+                                    alt="WhatsApp"
+                                    className={styles.icon}
+                                />
+                            </a>
                         </div>
                     </div>
+                </div>
+                <div className={`${styles.column} ${styles.web}`}>
+                    <Link href="/about" className={styles.link}>
+                        About Us
+                    </Link>
+                    <Link href="/product" className={styles.link}>
+                        Product
+                    </Link>
+                    <Link href="/services" className={styles.link}>
+                        Services
+                    </Link>
+                    <Link href="/contact" className={styles.link}>
+                        Contact
+                    </Link>
+                </div>
+                <div className={`${styles.column} ${styles.hidden}`}>
+                    <h3 className={styles.title}>Address</h3>
+                    <div className={styles.address}>Main Office & Factory</div>
+                    <div className={styles.important}>Istanbul Türkiye</div>
+                    <div className={styles.address}>
+                        Craft Industrial Design
+                    </div>
+                    <div className={styles.address}>Başakşehir Mah</div>
+                </div>
 
-                    <div
-                        className={`${styles.footerContainer} ${styles.secondContainer} `}
-                    >
-                        <Link href="/about" className={styles.footerLink}>
-                            About Us
-                        </Link>
-                        <Link href="/product" className={styles.footerLink}>
-                            Product
-                        </Link>
-                        <Link href="/services" className={styles.footerLink}>
-                            Services
-                        </Link>
-                        <Link href="/contact" className={styles.footerLink}>
-                            Contact
-                        </Link>
+                <div className={`${styles.column} ${styles.hidden}`}>
+                    <h3 className={styles.title}>Contact</h3>
+                    <div>P: +90 534 787 5334</div>
+                    <div>M: info@crafttasarim.com</div>
+                    <div className={styles.important}>for Export</div>
+                    <div>help@crafttasarim.com</div>
+                </div>
+                <div className={styles.rightContainer}>
+                    <div className={styles.column}>
+                        <h3 className={styles.title}>Address</h3>
+                        <div className={styles.address}>
+                            Main Office & Factory
+                        </div>
+                        <div className={styles.important}>Istanbul Türkiye</div>
+                        <div className={styles.address}>
+                            Craft Industrial Design
+                        </div>
+                        <div className={styles.address}>Başakşehir Mah</div>
+                    </div>
+
+                    <div className={styles.column}>
+                        <h3 className={styles.title}>Contact</h3>
+                        <div>P: +90 534 787 5334</div>
+                        <div>M: info@crafttasarim.com</div>
+                        <div className={styles.important}>for Export</div>
+                        <div>help@crafttasarim.com</div>
                     </div>
                 </div>
-                <div className={`${styles.secondContainer} `}>
-                    <div className={styles.footerColumn}>
-                        <h3 className={styles.footerTitle}>Address</h3>
-                        <p>Main Office & Factory</p>
-                        <p>Istanbul Türkiye</p>
-                        <p>Craft Industrial Design</p>
-                        <p>Başakşehir Mah</p>
-                    </div>
-
-                    <div className={styles.footerColumn}>
-                        <h3 className={styles.footerTitle}>Contact</h3>
-                        <p>P : +90 532 787 5334</p>
-                        <p>M : info@crafttasarim.com</p>
-                        <p>for Export</p>
-                        <p>help@crafttasarim.com</p>
-                    </div>
+                <div className={`${styles.column} ${styles.mobile}`}>
+                    <Link href="/about" className={styles.link}>
+                        About Us
+                    </Link>
+                    <Link href="/product" className={styles.link}>
+                        Product
+                    </Link>
+                    <Link href="/services" className={styles.link}>
+                        Services
+                    </Link>
+                    <Link href="/contact" className={styles.link}>
+                        Contact
+                    </Link>
                 </div>
             </div>
         </footer>
